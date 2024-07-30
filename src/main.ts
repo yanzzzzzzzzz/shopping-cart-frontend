6e54f6125ea33e6228ec16893e01c5fea2fc7370
mintsai7777 <king840110@gmail.com>
2024-07-30 23:16:22 +0800

    add Preset & setting darkModeSelector

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,35 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import router from './router';
 import './style.css';
+import { definePreset } from '@primevue/themes';
+
 const app = createApp(App);
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}',
+    },
+  },
+});
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MyPreset,
+    options: {
+      prefix: 'p',
+      darkModeSelector: '.my-app-dark',
+      cssLayer: false,
+    },
   },
 });
 app.use(router);
