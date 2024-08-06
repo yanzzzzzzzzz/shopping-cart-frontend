49a1424c3a9f7a898c7016bee1bd014a59a6c5f8
Jack Tsai <jtsai@nexify.com.hk>
2024-08-06 18:02:39 +0800

    add product detail

---
+++ src/components/ProductInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex bg-white mt-4">
+    <section class="p-2" style="width: 450px">
+      <Image
+        src="https://down-tw.img.susercontent.com/file/tw-11134207-7r992-lycg4m1xr0ms69"
+        alt="Image"
+        width="450"
+        preview
+      />
+    </section>
+    <h2>
+      【BODY GOALS 】多效乳清蛋白飲 bodygoals 乳清蛋白飲 植物性蛋 高蛋白
+      豌豆蛋白 蛋白飲 素食高蛋白
+    </h2>
+  </section>
+</template>
+<script setup lang="ts">
+import Image from 'primevue/image';
+</script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,6 +1,10 @@
 <template>
-  <ProductBreadcrumb />
+  <div>
+    <ProductBreadcrumb />
+    <ProductInfo />
+  </div>
 </template>
 <script setup lang="ts">
 import ProductBreadcrumb from '@/components/ProductBreadcrumb.vue';
+import ProductInfo from '@/components/ProductInfo.vue';
 </script>
