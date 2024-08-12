c03cc20ba27f12833589d5c439d775b6d632e929
Jack Tsai <jtsai@nexify.com.hk>
2024-08-12 16:32:18 +0800

    Add product spec

---
+++ src/components/ProductSpec.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg-white mt-3 p-5">
+    <div class="title">商品規格</div>
+    <div class="pt-2 m-2">
+      <div class="subtitle">
+        <label class="spec-label">分類</label>
+        <span>乳清蛋白</span>
+      </div>
+      <div class="subtitle">
+        <label class="spec-label">商品數量</label>
+        <span>2647</span>
+      </div>
+      <div class="subtitle">
+        <label class="spec-label">品牌</label>
+        <span>BODY GOALS</span>
+      </div>
+      <div class="subtitle">
+        <label class="spec-label">產地</label>
+        <span> 台灣</span>
+      </div>
+    </div>
+    <div class="title">商品描述</div>
+    <div class="pt-2 m-2">{{ product.describe }}</div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const product = ref({
+  describe: 'Body Goals 乳清蛋白飲 全素多效豌豆蛋白飲 31g',
+});
+</script>
+<style scoped>
+.spec-label {
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 0.875rem;
+  padding-right: 0.75rem;
+  width: 8.75rem;
+  display: inline-block;
+}
+.subtitle {
+  margin-bottom: 1rem;
+}
+.title {
+  font-size: 1.125rem;
+  background-color: #fafafa;
+  padding: 1rem 0.5rem;
+}
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,10 +3,12 @@
     <ProductBreadcrumb />
     <ProductInfo />
     <ProdcutShop />
+    <ProductSpec />
   </div>
 </template>
 <script setup lang="ts">
 import ProductBreadcrumb from '@/components/ProductBreadcrumb.vue';
 import ProductInfo from '@/components/ProductInfo.vue';
 import ProdcutShop from '@/components/ProdcutShop.vue';
+import ProductSpec from '@/components/ProductSpec.vue';
 </script>
