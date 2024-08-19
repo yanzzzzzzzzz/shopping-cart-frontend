abab55627a35e08c743ec9f0da75bb5b983e9d1f
Jack Tsai <jtsai@nexify.com.hk>
2024-08-19 12:49:57 +0800

    ProductDescription add title

--- src/components/ProductDescription.vue
+++ src/components/ProductDescription.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="bg-white p-5" v-html="desc"></div>
+  <div class="bg-white p-5">
+    <div class="title">商品描述</div>
+    <div class="m-3" v-html="desc"></div>
+  </div>
 </template>
 <script setup lang="ts">
 defineProps<{
   desc: string | undefined;
 }>();
 </script>
+<style scoped>
+.title {
+  font-size: 1.125rem;
+  background-color: #fafafa;
+  padding: 1rem 0.5rem;
+}
+</style>
