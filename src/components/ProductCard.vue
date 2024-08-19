5e331261b3cf5a1cb0ba09de8a36af5cc313cf95
Jack Tsai <jtsai@nexify.com.hk>
2024-08-19 11:16:16 +0800

    refactor remove unused import

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -23,7 +23,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
 import Card from 'primevue/card';
 import { Product } from '@/Model/type';
 import { useRouter } from 'vue-router';
