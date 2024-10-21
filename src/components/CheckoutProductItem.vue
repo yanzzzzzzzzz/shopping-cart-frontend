2b1200e19467e2316546cedd5a4d0d9ea7953ad6
Jack Tsai <jtsai@nexify.com.hk>
2024-10-21 12:49:35 +0800

    Remove unused

--- src/components/CheckoutProductItem.vue
+++ src/components/CheckoutProductItem.vue
@@ -25,7 +25,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
 import { CartItem } from '@/Model/type';
 import Image from 'primevue/image';
 const props = defineProps<{
