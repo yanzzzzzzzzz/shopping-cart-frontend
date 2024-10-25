b50311319a3d25812f6e91e78d2705f80921fcdb
Jack Tsai <jtsai@nexify.com.hk>
2024-10-25 18:01:51 +0800

    feat: credit card payment method

--- src/components/PaymentMethodSelector.vue
+++ src/components/PaymentMethodSelector.vue
@@ -14,9 +14,42 @@
       </div>
     </div>
     <div class="p-5 border-setting">
-      <div class="flex">
+      <div v-if="selectedPayment == '貨到付款'" class="flex">
         <div class="w-2">貨到付款</div>
         <div>現付</div>
+      </div>
+      <div v-if="selectedPayment == '信用卡'" class="flex">
+        <div class="w-2">選擇付款帳戶</div>
+        <div>
+          <div class="mb-4">卡片詳情</div>
+          <FloatLabel variant="on" class="mb-4">
+            <InputText v-model="cardNo" />
+            <label>信用卡號碼</label>
+          </FloatLabel>
+
+          <div class="flex mb-4">
+            <FloatLabel class="pr-3">
+              <InputText v-model="expiredDate" />
+              <label>到期日(MM/YY)</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText v-model="cvc" />
+              <label>安全驗證碼</label>
+            </FloatLabel>
+          </div>
+          <FloatLabel class="mb-4">
+            <InputText v-model="name" />
+            <label>持卡者名稱</label>
+          </FloatLabel>
+          <FloatLabel class="mb-4">
+            <InputText v-model="address" />
+            <label>帳單地址</label>
+          </FloatLabel>
+          <FloatLabel class="mb-4">
+            <InputText v-model="zipCode" />
+            <label>郵遞區號</label>
+          </FloatLabel>
+        </div>
       </div>
     </div>
     <div class="px-3 py-3 border-setting summary-section">
@@ -37,12 +70,20 @@
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
 const props = defineProps<{
   totalPrice: number;
 }>();
 const typeOfPayments = ref(['貨到付款', '信用卡']);
-const selectedPayment = ref('貨到付款');
+const selectedPayment = ref('信用卡');
 const shippingFee = ref(45);
+const cardNo = ref('');
+const expiredDate = ref('');
+const cvc = ref('');
+const name = ref('');
+const address = ref('');
+const zipCode = ref('');
 </script>
 <style scoped>
 .type-button {
